8225495543904c16de6a7327f49dbeb163d88a63
PuliAdrea <puli1515@gmail.com>
2024-09-14 22:46:10 -0500

    Se adicionan carpeta de scripts

---
+++ scripts/CrearProcedimientosAlmacenados.sql
@@ -0,0 +1,52 @@
+CREATE PROCEDURE sp_CreateUser
+    @FirstName VARCHAR(100),
+    @LastName VARCHAR(100),
+    @Email VARCHAR(150),
+    @Phone VARCHAR(20)
+AS
+BEGIN
+    INSERT INTO users (first_name, last_name, email, phone)
+    VALUES (@FirstName, @LastName, @Email, @Phone);
+END
+GO
+
+
+CREATE PROCEDURE sp_GetLast10Users
+AS
+BEGIN
+    SELECT TOP 10 * FROM users ORDER BY user_id DESC;
+END
+GO
+
+CREATE PROCEDURE sp_UpdateUser
+    @UserId INT,
+    @FirstName VARCHAR(100),
+    @LastName VARCHAR(100),
+    @Email VARCHAR(150),
+    @Phone VARCHAR(20)
+AS
+BEGIN
+    UPDATE users
+    SET first_name = @FirstName, last_name = @LastName, email = @Email, phone = @Phone
+    WHERE user_id = @UserId;
+END
+GO
+
+
+CREATE PROCEDURE sp_GetDepartments
+AS
+BEGIN
+    SELECT * FROM departments;
+END
+GO
+
+
+CREATE PROCEDURE sp_AssignUserToDepartment
+    @UserId INT,
+    @DepartmentId INT
+AS
+BEGIN
+    DELETE FROM user_department WHERE user_id = @UserId;  -- Eliminar asignación anterior
+    INSERT INTO user_department (user_id, department_id) VALUES (@UserId, @DepartmentId);
+END
+GO
